44e1b3536d6ba44be41ebc6e6c11b026fd51d1f3
Andrea Parenti <parenti@xfel.eu>
2015-07-14 12:40:47 +0000

    Install image_processing to site-packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='image_processing',
+      version='1.0',
+      description='Image processing utils',
+      author='Andrea Parenti',
+      author_email='andrea.parenti@xfel.eu',
+      url='http://www.xfel.eu/',
+      packages=['image_processing'],
+      )
