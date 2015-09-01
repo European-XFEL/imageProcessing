8002238968c544782f0479135ac88f93f70688e6
Andrea Parenti <parenti@xfel.eu>
2015-09-01 14:36:07 +0000

    Updated version to 2.1 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='image_processing',
-      version='1.0',
+      version='2.1',
       description='Image processing utils',
       author='Andrea Parenti',
       author_email='andrea.parenti@xfel.eu',
