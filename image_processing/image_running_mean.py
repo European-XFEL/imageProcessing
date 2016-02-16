603b68b14b74268b74890fba5c450a8c4e7084ed
Andrea Parenti <parenti@xfel.eu>
2016-02-16 12:41:49 +0000

    ImageRunningMean class: - shape, size, runningMean are now properties (were methods) - additional (optional) parameter maxlen to append method:   excess images are automatically popped when queue length exceeds maxlen.   More efficient than appending and later popping (average is updated once   instead of twice).

--- image_processing/image_running_mean.py
+++ image_processing/image_running_mean.py
@@ -10,26 +10,46 @@ from collections import deque
 class ImageRunningMean:
 
     def __init__(self):
-        self.__imageQueue = deque([])  # Image queue
         self.__runningMean = None  # Image running mean
+        self.__imageQueue = deque([])  # Image queue
 
-    def append(self, image):
+    def append(self, image, maxlen=None):
         '''Append an image to the queue, update running mean'''
 
         if not isinstance(image, numpy.ndarray):
             raise ValueError("image has incorrect type: %s" % str(type(image)))
 
-        _n = len(self.__imageQueue)
+        if maxlen is not None and maxlen<1:
+            raise ValueError("maxlen must be positive but is equal %s" % maxlen)
+
+        _n = len(self.__imageQueue)  # current length
         imageCopy = numpy.copy(image.astype('float64'))  # Make a copy of the image
+
         if _n==0:
             self.__imageQueue.append(imageCopy)
-            self.__runningMean = imageCopy  # Mean coincides with current image
-        else:
-            if imageCopy.shape==self.shape():
-                self.__runningMean = ( _n* self.__runningMean + imageCopy) / (_n+1)
-                self.__imageQueue.append(imageCopy)
+            self.__runningMean = imageCopy.copy()  # Mean coincides with current image
+            return
+
+        if imageCopy.shape!=self.shape:
+            raise ValueError("image has incorrect shape: %s != %s" % (str(imageCopy.shape), str(self.shape)))
+
+        _sum = _n*self.__runningMean
+        if maxlen is not None and _n>=maxlen:
+            # Images must be dropped
+
+            if _n<2*(maxlen-1):
+                # Pop images from queue and update running total
+                while len(self.__imageQueue)>=maxlen:
+                    _sum -= self.__imageQueue.popleft()
             else:
-                raise ValueError("image has incorrect shape: %s != %s" % (str(imageCopy.shape), str(self.shape())))
+                # More convenient to re-calculate sum
+                while len(self.__imageQueue)>=maxlen:
+                    self.__imageQueue.popleft()
+                _sum = sum(self.__imageQueue)
+            
+        # Append new image and update running mean
+        self.__imageQueue.append(imageCopy)
+        self.__runningMean = (_sum + imageCopy) / len(self.__imageQueue)
 
     def popleft(self):
         '''Pop an image from the queue, update the running mean'''
@@ -61,17 +81,20 @@ class ImageRunningMean:
         if _n>0:
             self.__runningMean = sum(self.__imageQueue) / _n
 
+    @property
     def runningMean(self):
         '''Return the running Mean'''
         return self.__runningMean
 
+    @property
     def size(self):
         '''Return the size of the queue'''
         return len(self.__imageQueue)
 
+    @property
     def shape(self):
         '''Return the shape of images in the queue'''
-        if self.size()==0:
+        if self.size==0:
             return ()
         else:
             return self.__imageQueue[0].shape
