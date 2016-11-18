ace1adfdd9ef9bdf589a5ec1b58be9d73174965f
Andrea Parenti <andrea.parenti@xfel.eu>
2016-11-18 13:13:38 +0100

    Reformatted according to Kerstin's suggestions

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -144,31 +144,23 @@ def imageCentreOfMass(image):
         raise ValueError("Image type is %r, must be np.ndarray" %
                          type(image))
 
-    if image.ndim == 1:
-        # 1-D image
-
+    if image.ndim == 1:  # 1-D image
         values = np.arange(image.size)
         weights = image
-
         # Centre-of-mass and width
         x0 = np.average(values, weights=weights)
         sx = np.average((values - x0) ** 2, weights=weights)
         sx = np.sqrt(sx)
+        return x0, sx
 
-        return (x0, sx)
-
-    elif image.ndim == 2:
-        # 2-D image
-
+    elif image.ndim == 2:  # 2-D image
         # sum over y, evaluate centre-of-mass and width
         imgX = image.sum(axis=0)
-        (x0, sx) = imageCentreOfMass(imgX)
-
+        x0, sx = imageCentreOfMass(imgX)
         # sum over x, evaluate centre-of-mass and width
         imgY = image.sum(axis=1)
         (y0, sy) = imageCentreOfMass(imgY)
-
-        return (x0, y0, sx, sy)
+        return x0, y0, sx, sy
 
     else:
         raise ValueError("Image dimensions are %d, must be 1 or 2" %
@@ -182,25 +174,25 @@ def fitGauss(image, p0=None, enablePolynomial=False):
         raise ValueError("Image type is %r, must be np.ndarray" %
                          type(image))
 
-    if image.ndim == 1:
-        # 1-D image
-
+    if image.ndim == 1:  # 1-D image
         if p0 is None:
             # Evaluates initial parameters
-            (x0, sx) = imageCentreOfMass(image)
+            x0, sx = imageCentreOfMass(image)
             if enablePolynomial is False:
                 p0 = (image.max(), x0, sx)
             else:
                 p0 = (image.max(), x0, sx, 0., 0.)
 
-        # [AP] scipy.optimize.leastsq assumes equal errors
         x = np.arange(image.size)
-        errFun = lambda p: gauss1d(x, *p, enablePolynomial=enablePolynomial)\
-            - image
-        out = scipy.optimize.leastsq(errFun, p0, full_output=1)
+        # [AP] scipy.optimize.leastsq assumes equal errors
+        out = scipy.optimize.leastsq(
+            lambda p: gauss1d(x, *p, enablePolynomial=enablePolynomial) -
+            image, p0, full_output=1
+        )
 
-        s_sq = (out[2]['fvec'] ** 2).sum() /\
-               (len(out[2]['fvec']) - len(out[0]))  # residual variance
+        fvec = out[2]['fvec']
+        s_sq = (fvec ** 2).sum() /\
+               (len(fvec) - len(out[0]))  # residual variance
         p1 = out[0]  # parameters
         if out[1] is not None:
             p1cov = s_sq * out[1]  # parameters covariance matrix
@@ -210,27 +202,27 @@ def fitGauss(image, p0=None, enablePolynomial=False):
 
         return p1, p1cov, ier
 
-    elif image.ndim == 2:
-        # 2-D image
-
+    elif image.ndim == 2:  # 2-D image
         if p0 is None:
             # Evaluates initial parameters
-            (x0, y0, sx, sy) = imageCentreOfMass(image)
+            x0, y0, sx, sy = imageCentreOfMass(image)
             if enablePolynomial is False:
                 p0 = (image.max(), x0, y0, sx, sy)
             else:
                 p0 = (image.max(), x0, y0, sx, sy, 0., 0., 0.)
 
-        # [AP] scipy.optimize.leastsq assumes equal errors
         x = np.arange(image.shape[1]).reshape(1, image.shape[1])
         y = np.arange(image.shape[0]).reshape(image.shape[0], 1)
-        errFun = lambda p: np.ravel(
-            gauss2d(x, y, *p, enablePolynomial=enablePolynomial) - image
+        # [AP] scipy.optimize.leastsq assumes equal errors
+        out = scipy.optimize.leastsq(
+            lambda p: np.ravel(
+                gauss2d(x, y, *p, enablePolynomial=enablePolynomial) - image),
+            p0, full_output=1
         )
-        out = scipy.optimize.leastsq(errFun, p0, full_output=1)
 
-        s_sq = (out[2]['fvec'] ** 2).sum() /\
-               (len(out[2]['fvec']) - len(out[0]))  # residual variance
+        fvec = out[2]['fvec']
+        s_sq = (fvec ** 2).sum() /\
+               (len(fvec) - len(out[0]))  # residual variance
         p1 = out[0]  # parameters
         if out[1] is not None:
             p1cov = s_sq * out[1]  # parameters covariance matrix
@@ -256,22 +248,24 @@ def fitGauss2DRot(image, p0=None, enablePolynomial=False):
 
     if p0 is None:
         # Evaluates initial parameters
-        (x0, y0, sx, sy) = imageCentreOfMass(image)
+        x0, y0, sx, sy = imageCentreOfMass(image)
         if enablePolynomial is False:
             p0 = (image.max(), x0, y0, sx, sy, 0.)
         else:
             p0 = (image.max(), x0, y0, sx, sy, 0., 0., 0., 0.)
 
-    # [AP] scipy.optimize.leastsq assumes equal errors
     x = np.arange(image.shape[1]).reshape(1, image.shape[1])
     y = np.arange(image.shape[0]).reshape(image.shape[0], 1)
-    errFun = lambda p: np.ravel(
-        gauss2dRot(x, y, *p, enablePolynomial=enablePolynomial) - image
+    # [AP] scipy.optimize.leastsq assumes equal errors
+    out = scipy.optimize.leastsq(
+        lambda p: np.ravel(
+            gauss2dRot(x, y, *p, enablePolynomial=enablePolynomial) - image),
+        p0, full_output=1
     )
-    out = scipy.optimize.leastsq(errFun, p0, full_output=1)
 
-    s_sq = (out[2]['fvec'] ** 2).sum() \
-        / (len(out[2]['fvec']) - len(out[0]))  # residual variance
+    fvec = out[2]['fvec']
+    s_sq = (fvec ** 2).sum() \
+        / (len(fvec) - len(out[0]))  # residual variance
     p1 = out[0]  # parameters
     if out[1] is not None:
         p1cov = s_sq * out[1]  # parameters covariance matrix
@@ -286,16 +280,16 @@ def gauss1d(x, height, x0, sx, a=0., b=0., enablePolynomial=False):
     """Returns a gaussian + 1st order polynomial, with the given parameters.
     The gaussian is in the form height*np.exp(-(((x-x0)/sx)**2)/2).
     The polynomial is in the form a*x + b."""
-    #
+
     f = height * np.exp(-0.5 * ((x - float(x0)) / sx) ** 2)
-    #
+
     if enablePolynomial is True:
         # Add polynomial
         if a != 0.:
             f += a * x  # In-place
         if b != 0:
             f += b  # In-place
-    #
+
     return f
 
 
@@ -305,11 +299,11 @@ def gauss2d(x, y, height, x0, y0, sx, sy, a=0., b=0., c=0.,
     The gaussian is in the form:
         height*np.exp(-(((x-x0)/sx)**2+((y-y0)/sy)**2)/2).
     The polynomial is in the form a*x + b*y +c."""
-    #
+
     f = height * np.exp(
         -0.5 * (((x - float(x0)) / sx) ** 2 + ((y - float(y0)) / sy) ** 2)
     )
-    #
+
     if enablePolynomial is True:
         # Add polynomial
         if a != 0.:
@@ -318,7 +312,7 @@ def gauss2d(x, y, height, x0, y0, sx, sy, a=0., b=0., c=0.,
             f += b * y  # In-place
         if c != 0:
             f += c  # In-place
-    #
+
     return f
 
 
@@ -328,7 +322,7 @@ def gauss2dRot(x, y, height, x0, y0, sx, sy, theta_deg, a=0., b=0., c=0.,
     The gaussian is in the form:
         height*np.exp(-(((x'-x0)/sx)**2+((y'-y0)/sy)**2)/2).
     The polynomial is in the form a*x + b*y +c."""
-    #
+
     theta = math.radians(theta_deg)  # rad -> deg
     # Reference frame change: First translate origin to (x0, y0)...
     x1 = x - x0
@@ -336,7 +330,7 @@ def gauss2dRot(x, y, height, x0, y0, sx, sy, theta_deg, a=0., b=0., c=0.,
     # ... then rotate axes
     x2 = x1 * math.cos(theta) - y1 * math.sin(theta)
     y2 = x1 * math.sin(theta) + y1 * math.cos(theta)
-    #
+
     f = gauss2d(x2, y2, height, 0., 0., sx, sy, a, b, c, enablePolynomial)
-    #
+
     return f
--- image_processing/image_running_mean.py
+++ image_processing/image_running_mean.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+
 from collections import deque
 
 
@@ -10,7 +11,7 @@ class ImageRunningMean:
         self.__imageQueue = deque([])  # Image queue
 
     def append(self, image, maxlen=None):
-        '''Append an image to the queue, update running mean'''
+        """Append an image to the queue, update running mean"""
 
         if not isinstance(image, np.ndarray):
             raise ValueError("image has incorrect type: %s" % str(type(image)))
@@ -19,10 +20,10 @@ class ImageRunningMean:
             raise ValueError("maxlen must be positive but is equal %s" %
                              maxlen)
 
-        _n = len(self.__imageQueue)  # current length
+        _queueLength = len(self.__imageQueue)  # current length
         imageCopy = np.copy(image.astype('float64'))  # Copy the image
 
-        if _n == 0:
+        if _queueLength == 0:
             # Mean coincides with current image
             self.__imageQueue.append(imageCopy)
             self.__runningMean = imageCopy.copy()
@@ -32,11 +33,11 @@ class ImageRunningMean:
             raise ValueError("image has incorrect shape: %s != %s" %
                              (str(imageCopy.shape), str(self.shape)))
 
-        _sum = _n * self.__runningMean
-        if maxlen is not None and _n >= maxlen:
+        _sum = _queueLength * self.__runningMean
+        if maxlen is not None and _queueLength >= maxlen:
             # Images must be dropped
 
-            if _n < 2 * (maxlen - 1):
+            if _queueLength < 2 * (maxlen - 1):
                 # Pop images from queue and update running total
                 while len(self.__imageQueue) >= maxlen:
                     _sum -= self.__imageQueue.popleft()
@@ -51,49 +52,50 @@ class ImageRunningMean:
         self.__runningMean = (_sum + imageCopy) / len(self.__imageQueue)
 
     def popleft(self):
-        '''Pop an image from the queue, update the running mean'''
+        """Pop an image from the queue, update the running mean"""
 
-        _n = len(self.__imageQueue)
-        if _n == 0:
+        _queueLength = len(self.__imageQueue)
+        if _queueLength == 0:
             return
-        elif _n == 1:
+        elif _queueLength == 1:
             # No images will be left
             self.__imageQueue.clear()
             self.__runningMean = None
-        elif _n == 2:
+        elif _queueLength == 2:
             # Only one image will be left
             # Mean coincides with last image left
             self.__imageQueue.popleft()
             self.__runningMean = np.copy(self.__imageQueue[0])
         else:
             image = self.__imageQueue.popleft()
-            self.__runningMean = (_n * self.__runningMean - image) / (_n - 1)
+            self.__runningMean = (_queueLength * self.__runningMean - image)\
+                / (_queueLength - 1)
 
     def clear(self):
-        '''Clear the queue and reset the running mean'''
+        """Clear the queue and reset the running mean"""
         self.__imageQueue.clear()
         self.__runningMean = None
 
     def recalculate(self):
-        '''Recalculate the mean'''
+        """Recalculate the mean"""
 
-        _n = len(self.__imageQueue)
-        if _n > 0:
-            self.__runningMean = sum(self.__imageQueue) / _n
+        _queueLength = len(self.__imageQueue)
+        if _queueLength > 0:
+            self.__runningMean = sum(self.__imageQueue) / _queueLength
 
     @property
     def runningMean(self):
-        '''Return the running Mean'''
+        """Return the running Mean"""
         return self.__runningMean
 
     @property
     def size(self):
-        '''Return the size of the queue'''
+        """Return the size of the queue"""
         return len(self.__imageQueue)
 
     @property
     def shape(self):
-        '''Return the shape of images in the queue'''
+        """Return the shape of images in the queue"""
         if self.size == 0:
             return ()
         else:
