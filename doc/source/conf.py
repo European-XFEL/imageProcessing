d6aed55ca2a7f8e5b75a5c0e9db1923f8910d83e
Andrea Parenti <andrea.parenti@xfel.eu>
2018-12-17 14:44:09 +0100

    Bring documentation up-to-date

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -58,7 +58,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ImageProcessing'
-copyright = '2016, A. Parenti'
+copyright = '2018, European XFEL GmbH'
 author = 'A. Parenti'
 
 # The version info for the project you're documenting, acts as replacement for
